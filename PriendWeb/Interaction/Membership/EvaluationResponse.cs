82cbddcfde2f2a96f3e03074fa8f2f55f396678a
nda111 <48427099+nda111@users.noreply.github.com>
2020-05-10 22:32:33 +0900

    Updated: Razor Setup

---
+++ PriendWeb/Interaction/Membership/EvaluationResponse.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Npgsql;
+using System.Threading.Tasks;
+
+namespace PriendWeb.Interaction.Membership
+{
+    public sealed class EvaluationResponse : IResponse
+    {
+        private enum EResponse : byte
+        {
+            Verified        = 0,
+            NotVerified     = 1,
+            Unknown         = 2,
+            ServerError     = 3,
+        }
+
+        public string Path => "/ws/membership/evaluation";
+
+        public async Task Response(HttpContext context, WebSocketConnection conn, NpgsqlConnection npgConn)
+        {
+            await conn.ReceiveAsync();
+            string email = conn.TextMessage;
+
+            try
+            {
+                using (var cmd = npgConn.CreateCommand())
+                {
+                    cmd.CommandText = $"SELECT (verified) FROM account WHERE email='{email}';";
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            bool bVerified = reader.GetBoolean(0);
+
+                            if (bVerified)
+                            {
+                                await conn.SendByteAsync((byte)EResponse.Verified);
+                            }
+                            else
+                            {
+                                await conn.SendByteAsync((byte)EResponse.NotVerified);
+                            }
+                        }
+                        else
+                        {
+                            await conn.SendByteAsync((byte)EResponse.Unknown);
+                        }
+                    }
+                }
+            }
+            catch (NpgsqlException e)
+            {
+                await conn.SendByteAsync((byte)EResponse.ServerError);
+
+                throw e;
+            }
+        }
+    }
+}
---
+++ PriendWeb/Interaction/Membership/LoginResponse.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Npgsql;
+using PriendWeb.Data;
+using PriendWeb.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriendWeb.Interaction.Membership
+{
+    public sealed class LoginResponse : IResponse
+    {
+        private enum EResponse : byte
+        {
+            Ok = 0,
+            WrongPassword = 1,
+            UnknownEmail = 2,
+            ServerError = 3,
+        }
+
+        public string Path => "/ws/membership/login";
+
+        public async Task Response(HttpContext context, WebSocketConnection conn, NpgsqlConnection npgConn)
+        {
+            await conn.ReceiveAsync();
+            string email = conn.TextMessage;
+
+            await conn.ReceiveAsync();
+            string password = conn.TextMessage;
+            string hashedPassword = Account.Hash.Password(password);
+
+            try
+            {
+                using (var cmd = npgConn.CreateCommand())
+                {
+                    cmd.CommandText = $"SELECT (pw, id, name, weights_alert, birth_alert, event_alert, com_comment) FROM account WHERE email='{email}';";
+
+                    Account account = null;
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            string readPassword = reader.GetString(2);
+
+                            if (hashedPassword == readPassword)
+                            {
+                                string authToken = Account.Hash.AuthenticationToken(email, password);
+
+                                account = new Account(
+                                    id: reader.GetInt64(1),
+                                    name: reader.GetString(2),
+                                    email: email,
+                                    authToken: authToken,
+                                    settings: new Settings(
+                                        weightAlert: reader.GetBoolean(3),
+                                        birthdayAlert: reader.GetBoolean(4),
+                                        eventAlert: reader.GetBoolean(5),
+                                        commentAlert: reader.GetBoolean(6)));
+                            }
+                            else
+                            {
+                                await conn.SendByteAsync((byte)EResponse.WrongPassword);
+                            }
+                        }
+                        else
+                        {
+                            await conn.SendByteAsync((byte)EResponse.UnknownEmail);
+                        }
+                    }
+
+                    if (account != null) // on login success
+                    {
+                        cmd.CommandText = $"UPDATE account SET auth_token='{account.AuthenticationToken}' WHERE email='{email}';";
+                        cmd.ExecuteNonQuery();
+
+                        string jsonString = account.ToJson().ToString();
+
+                        await conn.SendByteAsync((byte)EResponse.Ok);
+                        await conn.SendTextAsync(jsonString);
+                    }
+                }
+            }
+            catch (NpgsqlException e)
+            {
+                await conn.SendByteAsync((byte)EResponse.ServerError);
+
+                throw e;
+            }
+        }
+    }
+}
